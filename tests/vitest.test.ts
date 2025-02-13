e4919a96270d6100ca2360744df4971cd4801fff
k4yr2 <kyrzkaya@gmail.com>
2025-02-14 02:27:14 +0300

    Update vitest test to import routePaths and pathKeys for keyContent validation

--- tests/vitest.test.ts
+++ tests/vitest.test.ts
@@ -1,7 +1,9 @@
 import { test } from 'vitest';
 
 test('keyContent', async (context) => {
+    const {routePaths} = await import('../src/lib/route/routePaths');
+    const {pathKeys} = await import('../src/lib/path/pathKeys');
     const {keyContent} = await import('../src/lib/key/keyContent');
 
-    keyContent("[345]");
+    console.log(routePaths("fsdfds[fd][asd]?[3454][:: fdfdls]?? ?? sdfsdf"));
 });
