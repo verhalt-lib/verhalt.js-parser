1bb6155963337f3eae47318c42bd2468e6a5524c
k4yr2 <kyrzkaya@gmail.com>
2025-02-13 22:30:47 +0300

    Refactor exports by consolidating them in src/index.ts and removing src/lib/index.ts

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export * from "./lib/key/keyContent";
+export * from "./lib/key/keyIndex";
+export * from "./lib/path/pathKeys";
+export * from "./lib/route/routePaths";
--- src/lib/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./key/keyContent";
-export * from "./key/keyIndex";
-export * from "./path/pathKeys";
-export * from "./route/routePaths";
