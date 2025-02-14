ff9abc4c8bcc6297d29c1c2b2b77c08f1a4bc328
k4yr2 <kyrzkaya@gmail.com>
2025-02-14 03:39:56 +0300

    Add keyIndex function to parse input strings into numbers or return null for undefined

---
+++ src/lib/key/keyIndex.ts
@@ -0,0 +1,9 @@
+export function keyIndex(input? : string) : number | string | null {
+    if(input === undefined) return null;
+
+    if(/^(([1-9][0-9]*)|0)/.test(input)) {
+        return parseInt(input);
+    }
+    
+    return input;
+}
