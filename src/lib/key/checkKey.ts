ccec12c37bc719c4c3d6f3912f93a83568a5df8e
k4yr2 <kyrzkaya@gmail.com>
2025-02-14 23:47:29 +0300

    Fix checkKey function to correctly validate key token character

--- src/lib/key/checkKey.ts
+++ src/lib/key/checkKey.ts
@@ -1,6 +1,6 @@
 export function checkKey(input: string) : void {
     checkKeyWithoutToken(input);
-    if([":", "."].includes(input[0])) throw new Error("[VERHALT-KEY]: Key token must be ':' or '.' character.");
+    if(![":", "."].includes(input[0])) throw new Error("[VERHALT-KEY]: Key token must be ':' or '.' character.");
 }
 
 export function checkKeyWithoutToken(input: string) : void {
