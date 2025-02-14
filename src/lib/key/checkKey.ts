2ae596f0a489900b90ae7946318f7c2ed81ab73b
k4yr2 <kyrzkaya@gmail.com>
2025-02-14 23:39:29 +0300

    Add checkKeyWithoutToken function and enhance checkKey for improved input validation

--- src/lib/key/checkKey.ts
+++ src/lib/key/checkKey.ts
@@ -1,4 +1,9 @@
 export function checkKey(input: string) : void {
+    checkKeyWithoutToken(input);
+    if([":", "."].includes(input[0])) throw new Error("[VERHALT-KEY]: Key token must be ':' or '.' character.");
+}
+
+export function checkKeyWithoutToken(input: string) : void {
     if(typeof input !== "string") throw new Error("[VERHALT-KEY]: Key must be string");
     if(input.length === 0) throw new Error("[VERHALT-KEY]: Key must contain something.");
 }
