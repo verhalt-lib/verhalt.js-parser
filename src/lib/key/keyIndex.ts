d2e1beb68074c92071dddfccc621fb17436a33b5
k4yr2 <kyrzkaya@gmail.com>
2025-02-14 03:24:33 +0300

    Refactor keyIndex function to accept a source model and content, returning null for undefined input and handling numeric parsing more robustly

--- src/lib/key/keyIndex.ts
+++ src/lib/key/keyIndex.ts
@@ -1,9 +1,18 @@
-export function keyIndex(input? : string) : number {
-    if(input === undefined) return -1;
+import { VerhaltModel } from "@verhalt/types/lib";
+import { Verhalt } from "verhalt";
 
-    if(/^(([1-9][0-9]*)|0)/.test(input)) {
-        return parseInt(input);
+export function keyIndex<TModel extends VerhaltModel>(source : TModel, content? : string) : number | null {
+    if(content === undefined) return null;
+
+    if(/^(([1-9][0-9]*)|0)/.test(content)) {
+        return parseInt(content);
     }
+ 
+    const value = Verhalt.model(source, content);
 
-    return -1;
+    if(typeof value === "number") {
+        return value;
+    }
+    
+    return null;
 }
