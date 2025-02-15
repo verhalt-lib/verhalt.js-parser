2c80a1b152dc1e4f8e2076b3c3689bda61b1a4a3
k4yr2 <kyrzkaya@gmail.com>
2025-02-15 09:40:24 +0300

    Improve error message in checkStep function for invalid input types

--- src/lib/step/checkStep.ts
+++ src/lib/step/checkStep.ts
@@ -1,3 +1,3 @@
 export function checkStep(input : string) {
-    if(!["string", "undefined"].includes(typeof input)) throw new Error("[VERHALT-STEP]: Step must be string or undefined" + typeof input);
+    if(!["string", "undefined"].includes(typeof input)) throw new Error("[VERHALT-STEP]: Step must be string or undefined. ");
 }
