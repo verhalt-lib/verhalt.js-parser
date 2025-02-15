d7f530fa51ba710b55947ec8e547dd374d8555ea
k4yr2 <kyrzkaya@gmail.com>
2025-02-15 09:40:57 +0300

    Add test cases for parseStep function in vitest

--- tests/vitest.test.ts
+++ tests/vitest.test.ts
@@ -4,8 +4,14 @@ test('keyContent', async (context) => {
     const {parseRoutePaths: routePaths} = await import('../src/lib/route/parseRoutePaths');
     const {parsePathKeys: pathKeys} = await import('../src/lib/path/parsePathKeys');
     const {parseKey: keyValue} = await import('../src/lib/key/parseKey');
+    const {parseStep } = await import('../src/lib/step/parseStep');
 
-    const key = keyValue(":gigi?[caga][43]?[df]??");
-    console.log(key?.head);
-    console.log(key?.body.indexes);
+    console.log(parseStep("aha"));
+    console.log(parseStep("aha?"));
+    console.log(parseStep("aha!"));
+    console.log(parseStep("{:cigu[:cigu[0]]}!"));
+    console.log(parseStep("{:{:cigu[12]}.sasa}?"));
+    console.log(parseStep("{124}!"));
+    console.log(parseStep("[12]?"));
+    console.log(parseStep("[haha]!"));
 });
